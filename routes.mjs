4bf3683592d4661924080fc39421637c0ff35411
eddiejpot <jamesedwardporter@gmail.com>
2021-08-20 19:37:06 +0800

    added get routes for menu items and images

--- routes.mjs
+++ routes.mjs
@@ -5,15 +5,24 @@ import initItemsController from './controllers/items.mjs';
 import initOrdersController from './controllers/orders.mjs';
 
 export default function routes(app) {
+  const ItemsController = initItemsController(db);
   const OrdersController = initOrdersController(db);
+
   app.post('/orders', OrdersController.create);
   app.get('/orders', OrdersController.index);
 
-  const ItemsController = initItemsController(db);
-  app.get('/items', ItemsController.index);
+  // get all menu items
+  app.get('/api/items', ItemsController.index);
+  // get menu item by id
+  app.get('/api/item/:id', ItemsController.find);
 
-  // special JS page. Include the webpack index.html file
-  app.get('/home', (request, response) => {
-    response.sendFile(resolve('dist', 'main.html'));
+  // get item image by id
+  app.get('/api/items/image/:id', (request, response) => {
+    const { id } = request.params;
+    response.sendFile(resolve('public', `products/${id}.jpg`));
+  });
+
+  app.get('/', (request, response) => {
+    response.send('Rocket Tea API');
   });
 }
