00234ee35cbb60bfac9c9ed35df2b69558e19f6b
eddiejpot <jamesedwardporter@gmail.com>
2021-08-20 19:36:49 +0800

    added get routes for menu items and images

--- controllers/items.mjs
+++ controllers/items.mjs
@@ -2,7 +2,19 @@ export default function initItemsController(db) {
   const index = async (request, response) => {
     try {
       const items = await db.Item.findAll();
+      console.log(items);
       response.send({ items });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const find = async (request, response) => {
+    const { id } = request.params;
+    try {
+      const item = await db.Item.findByPk(id);
+      console.log(item);
+      response.send({ item });
     } catch (error) {
       console.log(error);
     }
@@ -10,5 +22,6 @@ export default function initItemsController(db) {
 
   return {
     index,
+    find,
   };
 }
