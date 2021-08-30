56d60f74083f04424a86e4e8ef264fe6045b9b50
jokatty <jyotikattani@gmail.com>
2021-08-30 15:16:43 +0800

    add db username

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
-    username: 'eddiejpot',
-    // username: 'jyotikattani',
+    // username: 'eddiejpot',
+    username: 'jyotikattani',
     password: null,
     database: 'rocket_tea_development',
     host: '127.0.0.1',
